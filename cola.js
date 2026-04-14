3b9e6bf559bab01d96be1f72e4414c4750e68334
Alejandro <weimargonzales153@gmail.com>
2026-04-13 20:09:17 -0500

    cola: agregar funcion agregar

---
+++ cola.js
@@ -0,0 +1,9 @@
+// Array que guarda los números de la cola
+let cola = [];
+
+// Función para agregar un número a la cola
+function agregar() {
+  let numero = 10; // número de prueba
+  cola.push(numero); // agrega el número al final del array
+  console.log("Elemento agregado:", numero); // muestra el número agregado
+}
