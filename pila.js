0820ca2a9e3127531a273ec90a2ffec48277f17b
Alejandro <weimargonzales153@gmail.com>
2026-04-13 19:44:40 -0500

    pila: agregar función push

---
+++ pila.js
@@ -0,0 +1,9 @@
+// Array que guarda los objetos de la pila
+let pila = [];
+
+// Función para agregar un objeto a la pila
+function push() {
+  let persona = { Nombre: "Orlando", Edad: 30 }; // objeto de prueba
+  pila.push(persona); // agrega el objeto al final del array
+  console.log("Elemento agregado:", persona); // muestra el objeto agregado
+}
